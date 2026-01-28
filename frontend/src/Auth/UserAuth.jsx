2c94b6fb96fe37a5ed695b7723e414f21b7e6f2a
ananyajain10 <jainananya355@gmail.com>
2026-01-28 13:48:41 +0530

    test

--- frontend/src/Auth/UserAuth.jsx
+++ frontend/src/Auth/UserAuth.jsx
@@ -1,19 +1,26 @@
-import React, { Children, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/user.context";
 
-const UserAuth = ({children}) => {
+const UserAuth = ({ children }) => {
   const { user, loading } = useContext(UserContext);
-  if (loading){
-    return null;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login");
+    }
+  }, [loading, user, navigate]);
+
+  if (loading) {
+    return null; 
   }
 
-  if (!user){
-    return navigate("/login");
+  if (!user) {
+    return null; 
   }
- 
-  return <>
-   {children}
-  </>
+
+  return <>{children}</>;
 };
 
 export default UserAuth;
